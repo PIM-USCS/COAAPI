5e8de312fcb33ebe99b9b328db45e008e24c313b
Lucas <silvalucas1912@gmail.com>
2023-03-31 12:47:09 -0300

    criado rota para regimes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import clientesRouter from "@modules/clientes/routes/clientes.routes";
+import regimeRouter from "@modules/regime/routes/regime.routes";
 import sessionsRouter from "@modules/usuarios/routes/sessions.routes";
 import usuariosRouter from "@modules/usuarios/routes/usuarios.routes";
 
@@ -9,6 +10,7 @@ const routes = Router();
 routes.use("/clientes", clientesRouter);
 routes.use("/usuarios", usuariosRouter);
 routes.use("/sessions", sessionsRouter);
+routes.use("/regimes", regimeRouter);
 
 routes.get("/", (request, response) => {
   return response.json({ message: "Hello Dev!" });
